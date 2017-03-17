c1f600cae602d15afcf7f1f50c1c214d99be3103
aarohan-iitrpr <chhajerpratik07@gmail.com>
2017-03-17 16:11:10 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -98,6 +98,23 @@ $(document).ready(function(){
 		$("#VolleyballW_content").hide();
         $("#gallery_content").hide();
     });
+    $("#BadmintonM_button1").click(function(){
+        $("#home_content").hide();
+        $("#event_content").hide();
+        $("#result_content").hide();
+        $("#team_content").hide();
+        $("#BadmintonM_content").show();
+        $("#BadmintonW_content").hide();
+        $("#Basketball_content").hide();
+        $("#Cricket_content").hide();
+        $("#Football_content").hide();
+        $("#Lawn_content").hide();
+        $("#TableM_content").hide();
+        $("#TableW_content").hide();
+        $("#Volleyball_content").hide();
+        $("#VolleyballW_content").hide();
+        $("#gallery_content").hide();
+    });
     $("#BadmintonW_button").click(function(){
         $("#home_content").hide();
         $("#event_content").hide();
@@ -113,6 +130,23 @@ $(document).ready(function(){
         $("#TableW_content").hide();
         $("#Volleyball_content").hide();
 		$("#VolleyballW_content").hide();
+        $("#gallery_content").hide();
+    });
+    $("#BadmintonW_button1").click(function(){
+        $("#home_content").hide();
+        $("#event_content").hide();
+        $("#result_content").hide();
+        $("#team_content").hide();
+        $("#BadmintonM_content").hide();
+        $("#BadmintonW_content").show();
+        $("#Basketball_content").hide();
+        $("#Cricket_content").hide();
+        $("#Football_content").hide();
+        $("#Lawn_content").hide();
+        $("#TableM_content").hide();
+        $("#TableW_content").hide();
+        $("#Volleyball_content").hide();
+        $("#VolleyballW_content").hide();
         $("#gallery_content").hide();
     });
     $("#Basketball_button").click(function(){
@@ -132,6 +166,23 @@ $(document).ready(function(){
 		$("#VolleyballW_content").hide();
         $("#gallery_content").hide();
     });
+    $("#Basketball_button1").click(function(){
+        $("#home_content").hide();
+        $("#event_content").hide();
+        $("#result_content").hide();
+        $("#team_content").hide();
+        $("#BadmintonM_content").hide();
+        $("#BadmintonW_content").hide();
+        $("#Basketball_content").show();
+        $("#Cricket_content").hide();
+        $("#Football_content").hide();
+        $("#Lawn_content").hide();
+        $("#TableM_content").hide();
+        $("#TableW_content").hide();
+        $("#Volleyball_content").hide();
+        $("#VolleyballW_content").hide();
+        $("#gallery_content").hide();
+    });
     $("#Cricket_button").click(function(){
         $("#home_content").hide();
         $("#event_content").hide();
@@ -147,6 +198,23 @@ $(document).ready(function(){
         $("#TableW_content").hide();
         $("#Volleyball_content").hide();
 		$("#VolleyballW_content").hide();
+        $("#gallery_content").hide();
+    });
+    $("#Cricket_button1").click(function(){
+        $("#home_content").hide();
+        $("#event_content").hide();
+        $("#result_content").hide();
+        $("#team_content").hide();
+        $("#BadmintonM_content").hide();
+        $("#BadmintonW_content").hide();
+        $("#Basketball_content").hide();
+        $("#Cricket_content").show();
+        $("#Football_content").hide();
+        $("#Lawn_content").hide();
+        $("#TableM_content").hide();
+        $("#TableW_content").hide();
+        $("#Volleyball_content").hide();
+        $("#VolleyballW_content").hide();
         $("#gallery_content").hide();
     });
     $("#Football_button").click(function(){
@@ -166,6 +234,23 @@ $(document).ready(function(){
 		$("#VolleyballW_content").hide();
         $("#gallery_content").hide();
     });
+    $("#Football_button1").click(function(){
+        $("#home_content").hide();
+        $("#event_content").hide();
+        $("#result_content").hide();
+        $("#team_content").hide();
+        $("#BadmintonM_content").hide();
+        $("#BadmintonW_content").hide();
+        $("#Basketball_content").hide();
+        $("#Cricket_content").hide();
+        $("#Football_content").show();
+        $("#Lawn_content").hide();
+        $("#TableM_content").hide();
+        $("#TableW_content").hide();
+        $("#Volleyball_content").hide();
+        $("#VolleyballW_content").hide();
+        $("#gallery_content").hide();
+    });
     $("#Lawn_button").click(function(){
         $("#home_content").hide();
         $("#event_content").hide();
@@ -181,6 +266,23 @@ $(document).ready(function(){
         $("#TableW_content").hide();
         $("#Volleyball_content").hide();
 		$("#VolleyballW_content").hide();
+        $("#gallery_content").hide();
+    });
+    $("#Lawn_button1").click(function(){
+        $("#home_content").hide();
+        $("#event_content").hide();
+        $("#result_content").hide();
+        $("#team_content").hide();
+        $("#BadmintonM_content").hide();
+        $("#BadmintonW_content").hide();
+        $("#Basketball_content").hide();
+        $("#Cricket_content").hide();
+        $("#Football_content").hide();
+        $("#Lawn_content").show();
+        $("#TableM_content").hide();
+        $("#TableW_content").hide();
+        $("#Volleyball_content").hide();
+        $("#VolleyballW_content").hide();
         $("#gallery_content").hide();
     });
     $("#TableM_button").click(function(){
@@ -200,6 +302,23 @@ $(document).ready(function(){
 		$("#VolleyballW_content").hide();
         $("#gallery_content").hide();
     });
+    $("#TableM_button1").click(function(){
+        $("#home_content").hide();
+        $("#event_content").hide();
+        $("#result_content").hide();
+        $("#team_content").hide();
+        $("#BadmintonM_content").hide();
+        $("#BadmintonW_content").hide();
+        $("#Basketball_content").hide();
+        $("#Cricket_content").hide();
+        $("#Football_content").hide();
+        $("#Lawn_content").hide();
+        $("#TableM_content").show();
+        $("#TableW_content").hide();
+        $("#Volleyball_content").hide();
+        $("#VolleyballW_content").hide();
+        $("#gallery_content").hide();
+    });
     $("#TableW_button").click(function(){
         $("#home_content").hide();
         $("#event_content").hide();
@@ -215,6 +334,23 @@ $(document).ready(function(){
         $("#TableW_content").show();
         $("#Volleyball_content").hide();
 		$("#VolleyballW_content").hide();
+        $("#gallery_content").hide();
+    });
+    $("#TableW_button1").click(function(){
+        $("#home_content").hide();
+        $("#event_content").hide();
+        $("#result_content").hide();
+        $("#team_content").hide();
+        $("#BadmintonM_content").hide();
+        $("#BadmintonW_content").hide();
+        $("#Basketball_content").hide();
+        $("#Cricket_content").hide();
+        $("#Football_content").hide();
+        $("#Lawn_content").hide();
+        $("#TableM_content").hide();
+        $("#TableW_content").show();
+        $("#Volleyball_content").hide();
+        $("#VolleyballW_content").hide();
         $("#gallery_content").hide();
     });
     $("#Volleyball_button").click(function(){
@@ -234,6 +370,23 @@ $(document).ready(function(){
 		$("#VolleyballW_content").hide();
         $("#gallery_content").hide();
     });
+    $("#Volleyball_button1").click(function(){
+        $("#home_content").hide();
+        $("#event_content").hide();
+        $("#result_content").hide();
+        $("#team_content").hide();
+        $("#BadmintonM_content").hide();
+        $("#BadmintonW_content").hide();
+        $("#Basketball_content").hide();
+        $("#Cricket_content").hide();
+        $("#Football_content").hide();
+        $("#Lawn_content").hide();
+        $("#TableM_content").hide();
+        $("#TableW_content").hide();
+        $("#Volleyball_content").show();
+        $("#VolleyballW_content").hide();
+        $("#gallery_content").hide();
+    });
 	$("#VolleyballW_button").click(function(){
         $("#home_content").hide();
         $("#event_content").hide();
@@ -249,6 +402,23 @@ $(document).ready(function(){
         $("#TableW_content").hide();
         $("#Volleyball_content").hide();
 		$("#VolleyballW_content").show();
+        $("#gallery_content").hide();
+    });
+    $("#VolleyballW_button1").click(function(){
+        $("#home_content").hide();
+        $("#event_content").hide();
+        $("#result_content").hide();
+        $("#team_content").hide();
+        $("#BadmintonM_content").hide();
+        $("#BadmintonW_content").hide();
+        $("#Basketball_content").hide();
+        $("#Cricket_content").hide();
+        $("#Football_content").hide();
+        $("#Lawn_content").hide();
+        $("#TableM_content").hide();
+        $("#TableW_content").hide();
+        $("#Volleyball_content").hide();
+        $("#VolleyballW_content").show();
         $("#gallery_content").hide();
     });
     $("#gallery_button").click(function(){
